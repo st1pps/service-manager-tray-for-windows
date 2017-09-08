9b487268a72e2400df200be73fb48d252a414885
Fredrik Stipps <stipps@outlook.com>
2017-09-08 13:33:59 +0000

    Set up caliburn micro, created first UI elements

--- ServiceManager/ViewModels/ShellViewModel.cs
+++ ServiceManager/ViewModels/ShellViewModel.cs
@@ -1,8 +1,42 @@
-﻿using Caliburn.Micro;
+﻿using System.Diagnostics;
+using System.ServiceProcess;
+using System.Windows;
+using Caliburn.Micro;
 
 namespace ServiceManager.ViewModels
 {
-    public class ShellViewModel : PropertyChangedBase
+    public sealed class ShellViewModel : Conductor<ServiceViewModel>.Collection.OneActive
     {
+        public ShellViewModel()
+        {
+            DisplayName = "Service Manager";
+            Activated += (s,e) => RefreshServices();
+        }
+
+
+        public void RefreshServices()
+        {
+            var services = ServiceController.GetServices();
+            foreach (var s in services)
+            {
+                Items.Add(new ServiceViewModel(s));
+            }
+        }
+
+        public void OpenWindowsServiceManager()
+        {
+            Process.Start("services.msc");
+        }
+
+        public void OpenTaskManager()
+        {
+            Process.Start("taskmgr.exe");
+        }
+
+        public void Exit()
+        {
+            Application.Current.Shutdown(0);
+        }
+        
     }
 }
